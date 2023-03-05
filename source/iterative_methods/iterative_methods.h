06102203d064fde1d7b59591fe1ff5c279d52d59
Element <eg.zzz.747@gmail.com>
2023-03-05 10:51:05 +0300

    Iterative methods changed

--- source/iterative_methods/iterative_methods.h
+++ source/iterative_methods/iterative_methods.h
@@ -5,7 +5,7 @@
 
 #include "../csr_matrix/csr_matrix.h"
 
-std::vector<double> SolveIterativeMethod(const csr::CSRMatrix A,
+std::vector<double> SolveIterativeMethod(const csr::CSRMatrix &A,
                                          const std::vector<double> &b);
 
 #endif //SOLE_ITERATIVE_METHODS_H
